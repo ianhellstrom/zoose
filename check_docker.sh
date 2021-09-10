6cdd68aadd8603e2b8caf02bf03bb984256ea43f
Ian Hellström <ian.hellstrom@nubank.com.br>
2021-09-10 11:08:06 +0200

    Fix Hadolint issues

--- check_docker.sh
+++ check_docker.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker run --pull always --rm -i \
-  -v hadolint.yaml:/.config/hadolint.yaml \
-  hadolint/hadolint < docker/Dockerfile
+docker run --pull always --rm -i -v $(pwd)/hadolint.yaml:/.config/hadolint.yaml hadolint/hadolint < docker/Dockerfile
