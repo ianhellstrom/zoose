d023b26c36cacee076f3d3c8c86e2c3cf33f1144
Ian Hellström <ian@databaseline.tech>
2022-08-16 16:36:30 +0200

    Split into multiple Dockerfiles with docker-compose

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 source conf.sh
 docker build --build-arg zoose_version=$ZOOSE_TAG -t "$DOCKER_IMAGE" docker
+
+docker-compose up --build --detach
+docker-compose logs --tail 25
+docker compose down
--- check_docker.sh
+++ check_docker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-docker run --pull always --rm -i \
-  -v $(pwd)/hadolint.yaml:/.config/hadolint.yaml \
-  hadolint/hadolint < docker/Dockerfile
+for dir in *; do
+  if [ -d "${dir}" ]; then
+    echo "${dir}"
+    docker run --pull always --rm -i \
+      -v $(pwd)/hadolint.yaml:/.config/hadolint.yaml \
+      hadolint/hadolint < ${dir}/Dockerfile
+  fi
+done
