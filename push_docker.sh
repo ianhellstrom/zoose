8c958384152b83cd9631b2f830c9141e9da1a5f4
Ian Hellström <ian@databaseline.tech>
2022-08-17 13:44:13 +0200

    Add loop to tag latest images

--- push_docker.sh
+++ push_docker.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 docker compose push
+
+source .env
+for dir in *; do
+  if [ -d "${dir}" ]; then
+	docker tag "${PREFIX}-${dir}" "${PREFIX}-${dir}:latest"
+	docker push "${PREFIX}--${dir}:latest"
+  fi
+done
