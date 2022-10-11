a031a5da77f5edfc2333c7ad4a135aed8258905d
Ian Hellström <49023008+ianhellstrom@users.noreply.github.com>
2022-10-11 22:24:00 +0200

    Add special flavours

--- push_docker.sh
+++ push_docker.sh
@@ -4,9 +4,14 @@ docker compose push
 source .env
 for dir in *; do
   if [ -d "${dir}" ]; then
-  	if [ "${dir}" != "r" ]; then
-      docker tag "${PREFIX}-${dir}" "${PREFIX}-${dir}:latest"
+  	if [ "${dir}" != "gitpod" ]; then
+      docker tag  "${PREFIX}-${dir}" "${PREFIX}-${dir}:latest"
       docker push "${PREFIX}-${dir}:latest"
+    else
+      docker tag  "${PREFIX}-base-${dir}" "${PREFIX}-base-${dir}:latest"
+      docker push "${PREFIX}-base-${dir}:latest"
+      docker tag  "${PREFIX}-quantum-${dir}" "${PREFIX}-quantum-${dir}:latest"
+      docker push "${PREFIX}-quantum-${dir}:latest"
     fi
   fi
 done
