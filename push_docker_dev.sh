e7d04ee6712607fd74e3c63bb011687d2ee9d6cc
Ian Hellström <ian@databaseline.tech>
2022-10-12 13:18:44 +0200

    Add tag to dev-only images

--- push_docker_dev.sh
+++ push_docker_dev.sh
@@ -4,12 +4,12 @@ source .env
 for dir in *; do
   if [ -d "${dir}" ]; then
   	if [ "${dir}" != "gitpod" ]; then
-      docker tag  "${PREFIX}-${dir}" "${PREFIX}-${dir}:dev-only"
+      docker tag  "${PREFIX}-${dir}:${TAG}" "${PREFIX}-${dir}:dev-only"
       docker push "${PREFIX}-${dir}:dev-only"
     else
-      docker tag  "${PREFIX}-base-${dir}" "${PREFIX}-base-${dir}:dev-only"
+      docker tag  "${PREFIX}-base-${dir}:${TAG}" "${PREFIX}-base-${dir}:dev-only"
       docker push "${PREFIX}-base-${dir}:dev-only"
-      docker tag  "${PREFIX}-quantum-${dir}" "${PREFIX}-quantum-${dir}:dev-only"
+      docker tag  "${PREFIX}-quantum-${dir}:${TAG}" "${PREFIX}-quantum-${dir}:dev-only"
       docker push "${PREFIX}-quantum-${dir}:dev-only"
     fi
   fi
