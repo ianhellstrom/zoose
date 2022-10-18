ad9ec7e72b5e6a2e10248674f30d03df1461c456
Ian Hellström <ian@databaseline.tech>
2022-10-18 15:27:50 +0200

    Enable remote access

--- gitpod/jupyter_lab_config.py
+++ gitpod/jupyter_lab_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
 c.ServerApp.allow_password_change = False
-c.ServerApp.allow_remote_access = False
+c.ServerApp.allow_remote_access = True
 c.ServerApp.open_browser = False
 c.ServerApp.password = ''
 c.ServerApp.password_required = False
