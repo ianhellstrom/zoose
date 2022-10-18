3f9eefc440289d81a79380aed9fbf28da2dfb900
Ian Hellström <ian@databaseline.tech>
2022-10-18 16:05:11 +0200

    Simplify setup

--- gitpod/jupyter_lab_config.py
+++
@@ -1,8 +0,0 @@
-c = get_config()
-
-c.ServerApp.allow_password_change = False
-c.ServerApp.allow_remote_access = True
-c.ServerApp.open_browser = False
-c.ServerApp.password = ''
-c.ServerApp.password_required = False
-c.ServerApp.token = ''
