2e07280f39174207b818ae45eb1f3d3d13b7e99d
Ian Hellström <49023008+ianhellstrom@users.noreply.github.com>
2022-10-19 10:43:28 +0200

    Disable JupyterLab authentication in Gitpod images (#19)
    Fixes https://github.com/ianhellstrom/zoose/issues/18
    
    For Gitpod-based development, auth is handled by Gitpod, so we can
    'safely' disable password/token authentication:
    
    > Not recommended _unless_ authentication and access restrictions are
    handled at a different layer in the stack.
    See: https://jupyter-notebook.readthedocs.io/en/stable/security.html
    
    Check list:
    
    - [X] PR description is meaningful.
    - [X] `./check_docker` executes successfully locally.
    - [x] `./build_docker` executes successfully locally.
    - [x] Zoose version in `.env` has been updated appropriately.
    - [X] Commit has been tagged `git tag X.Y.Z` with `X.Y.Z` as in `.env`.
    - [ ] `README` has been updated (if applicable).

--- push_docker_dev.sh
+++ push_docker_dev.sh
@@ -4,13 +4,13 @@ source .env
 for dir in *; do
   if [ -d "${dir}" ]; then
   	if [ "${dir}" != "gitpod" ]; then
-      docker tag  "${PREFIX}-${dir}:${TAG}" "${PREFIX}-${dir}:dev-only"
-      docker push "${PREFIX}-${dir}:dev-only"
+      docker tag  "${PREFIX}-${dir}:${TAG}" "${PREFIX}-${dir}:dev-${TAG}"
+      docker push "${PREFIX}-${dir}:dev-${TAG}"
     else
-      docker tag  "${PREFIX}-base-${dir}:${TAG}" "${PREFIX}-base-${dir}:dev-only"
-      docker push "${PREFIX}-base-${dir}:dev-only"
-      docker tag  "${PREFIX}-quantum-${dir}:${TAG}" "${PREFIX}-quantum-${dir}:dev-only"
-      docker push "${PREFIX}-quantum-${dir}:dev-only"
+      docker tag  "${PREFIX}-base-${dir}:${TAG}" "${PREFIX}-base-${dir}:dev-${TAG}"
+      docker push "${PREFIX}-base-${dir}:dev-${TAG}"
+      docker tag  "${PREFIX}-quantum-${dir}:${TAG}" "${PREFIX}-quantum-${dir}:dev-${TAG}"
+      docker push "${PREFIX}-quantum-${dir}:dev-${TAG}"
     fi
   fi
 done
